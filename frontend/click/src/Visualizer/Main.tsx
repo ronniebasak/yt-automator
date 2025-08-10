12e704dadc5dd12d744365552ab137685bf59891
Umesh <kumesh2352@gmail.com>
2025-08-10 14:54:40 +0530

    remove spectrum and title and subtitle of audio

--- frontend/click/src/Visualizer/Main.tsx
+++ frontend/click/src/Visualizer/Main.tsx
@@ -1,32 +1,10 @@
 import React from "react";
 import { AbsoluteFill, Audio, Sequence, useVideoConfig } from "remotion";
 
-import { Spectrum } from "./Spectrum";
 import { AudiogramCompositionSchemaType } from "../helpers/schema";
-import { Waveform } from "./Waveform";
 import { BassOverlay } from "./BassOverlay";
-import { SongInfo } from "./SongInfo";
 import { Caption } from "./Caption";
 import { BackgroundVideo } from "./BackgroundVideo";
-import { FONT_FAMILY } from "../helpers/font";
-
-const containerStyle: React.CSSProperties = {
-  flexDirection: "column",
-  justifyContent: "flex-end",
-  color: "white",
-  padding: 48,
-  gap: 32,
-  fontFamily: FONT_FAMILY,
-};
-
-const visualizerContainerStyle: React.CSSProperties = {
-  display: "flex",
-  alignItems: "center",
-  width: "100%",
-  borderRadius: 32,
-  padding: 32,
-  marginTop: 32,
-};
 
 export const Visualizer: React.FC<AudiogramCompositionSchemaType> = ({
   visualizer,
@@ -57,32 +35,6 @@ export const Visualizer: React.FC<AudiogramCompositionSchemaType> = ({
       <Sequence from={-audioOffsetInFrames}>
         <BassOverlay audioSrc={audioFileUrl} color={visualizer.color} />
         <Audio pauseWhenBuffering src={audioFileUrl} />
-        <AbsoluteFill style={{...containerStyle, backgroundColor: "transparent", zIndex: 3}}>
-          <div style={{...visualizerContainerStyle, zIndex: 3}}>
-            {visualizer.type === "oscilloscope" ? (
-              <Waveform
-                waveColor={visualizer.color}
-                audioSrc={audioFileUrl}
-                windowInSeconds={visualizer.windowInSeconds}
-                amplitude={visualizer.amplitude}
-              />
-            ) : visualizer.type === "spectrum" ? (
-              <Spectrum
-                barColor={visualizer.color}
-                audioSrc={audioFileUrl}
-                mirrorWave={visualizer.mirrorWave}
-                numberOfSamples={Number(visualizer.numberOfSamples)}
-                waveLinesToDisplay={visualizer.linesToDisplay}
-              />
-            ) : null}
-          </div>
-          <SongInfo
-            coverImageUrl={coverImageUrl}
-            songName={songName}
-            artistName={artistName}
-            textColor={textColor}
-          />
-        </AbsoluteFill>
         {timestamps && timestamps.length > 0 && (
           <Caption timestamps={timestamps} textColor={textColor} />
         )}
